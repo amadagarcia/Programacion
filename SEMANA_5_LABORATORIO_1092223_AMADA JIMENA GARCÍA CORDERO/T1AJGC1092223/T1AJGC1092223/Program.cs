d6a2870f5588fc1ac1b1787c70d46ff01ebc409c
amadagarcia <141884511+amadagarcia@users.noreply.github.com>
2023-09-07 19:42:06 -0600

    Add files via upload

---
+++ SEMANA_5_LABORATORIO_1092223_AMADA JIMENA GARCÍA CORDERO/T1AJGC1092223/T1AJGC1092223/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Mi segundo programa");
+        Console.WriteLine("Ingrese su nombre: ");
+        string Nombre2 = Console.ReadLine();
+        Console.WriteLine("Ingrese su edad: ");
+        string Edad = Console.ReadLine();
+        Console.WriteLine("Ingrese su carrera: ");
+        string Carrera = Console.ReadLine();
+        Console.WriteLine("Ingrese su carné: ");
+        string Carné = Console.ReadLine();
+        /*COMENTARIOS;*/
+        Console.Write("Nombre: " + Nombre2);
+        Console.Write(" Edad: " + Edad);
+        Console.Write(" Carrera: " + Carrera);
+        Console.Write(" Carné: " + Carné);
+
+        Console.WriteLine(" Soy " + Nombre2 + " Tengo " + Edad + " años y estudio la carrera de " + Carrera + "."); 
+        Console.WriteLine("Mi número de carné es: " + Carné);
+
+        Console.ReadKey();
+    }
+}
