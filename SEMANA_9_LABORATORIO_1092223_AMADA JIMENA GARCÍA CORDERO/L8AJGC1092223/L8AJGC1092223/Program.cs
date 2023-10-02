5f2a7b683f845c082bc38a68506cc5cc7a5d96d9
amadagarcia <141884511+amadagarcia@users.noreply.github.com>
2023-10-02 10:06:58 -0600

    Add files via upload

---
+++ SEMANA_9_LABORATORIO_1092223_AMADA JIMENA GARCÍA CORDERO/L8AJGC1092223/L8AJGC1092223/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Laboratorio 9");
+        Console.WriteLine("Ejercicio #1");
+        Console.WriteLine("\nIngrese un número: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+        int contador = 1;
+        int sumatoria = 0;
+
+        while (contador <= numero)
+        {
+            sumatoria = sumatoria + contador;
+            contador++;
+
+        }
+
+        Console.WriteLine("Resultado: " + sumatoria);
+        Console.ReadKey();
+
+        Console.WriteLine("Ejercicio #2");
+        Console.WriteLine("FOR");
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write("\nTabla del " + i + ":\t");
+            for (int j = 1; j <= numero; j++)
+            {
+                Console.Write(i * j + "\t");
+            }
+        }
+        Console.ReadLine();
+    }
+}
