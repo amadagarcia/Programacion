98858bdff5515b07bc22a8f2250c2d3ec264f3fd
amadagarcia <141884511+amadagarcia@users.noreply.github.com>
2023-10-06 20:47:02 -0600

    Add files via upload

---
+++ SEMANA_9_TAREA_1092223_AMADA JIMENA GARCÍA CORDERO/T9_AJGC1092223/T9_AJGC1092223/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    //Amada García - 1092223
+    {
+        Console.WriteLine("Tarea 9");
+        Console.WriteLine("Primera operación");
+        Console.Write("Ingrese un número entero mayor a 0: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero <= 0)
+        {
+            Console.WriteLine("ERROR: El número que ingrese debe ser un entero mayor a 0.");
+        }
+        else
+        {
+            double suma1 = 0.0;
+            int a = 1;
+
+            while (a <= numero)
+            {
+                suma1 += 1.0 / a;
+                a++;
+            }
+            Console.WriteLine("El resultado de la primera operación es: " + suma1);
+        }
+
+        Console.WriteLine("Segunda operación");
+        if (numero <= 0)
+        {
+            Console.WriteLine("ERROR: El número que ingrese debe ser un entero mayor a 0");
+        }
+        else
+        {
+            double suma2 = 0.0;
+
+            for (int b = 1; b <= numero; b++)
+            {
+                suma2 += 1.0 / Math.Pow(2, b);
+
+            }
+            Console.WriteLine("El resultado de la segunda operación es: " + suma2);
+        }
+    }
+}
