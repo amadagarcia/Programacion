5bc4f866008b50b3b7a776413f242dbe0fb60e28
amadagarcia <141884511+amadagarcia@users.noreply.github.com>
2023-11-06 09:20:05 -0600

    Add files via upload

---
+++ SEMANA_14_LABORATORIO_1092223_AMADA JIMENA GARCÍA CORDERO/L14AJGC1092223/L14AJGC1092223/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese 7 números enteros: ");
+
+        int num = 0;
+        int cantPositiva = 0;
+        int cantNegativa = 0;
+        double numeros = 0;
+
+        for (int i = 0; i < 7; i++)
+        {
+            Console.Write("Número " + (i + 1));
+            if (int.TryParse(Console.ReadLine(), out int num))
+            {
+                numeros[i] = num;
+
+                if (num > mayor)
+                    mayor = num;
+                if (num < menor)
+                    menor = num;
+                if (num > 0)
+                    cantPositiva++;
+                else if (num < 0)
+                    cantNegativa++;
+            }
+        }
+    }
+}
